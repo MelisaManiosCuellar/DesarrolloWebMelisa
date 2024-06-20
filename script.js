1fdce5bd79816910c29c883a72b033659edc5865
Melisa Manios <melisamanios0905@gmail.com>
2024-06-19 20:31:08 -0500

    js

--- script.js
+++ script.js
@@ -10,26 +10,4 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 
-    // Funcionalidad del carrusel
-    const carouselInner = document.querySelector('.carousel__inner');
-    const prevButton = document.querySelector('.carousel__button--prev');
-    const nextButton = document.querySelector('.carousel__button--next');
-    const items = document.querySelectorAll('.carousel__item');
-    const totalItems = items.length;
-    let currentIndex = 0;
-
-    function updateCarousel() {
-        const offset = -currentIndex * 100;
-        carouselInner.style.transform = `translateX(${offset}%)`;
-    }
-
-    prevButton.addEventListener('click', () => {
-        currentIndex = (currentIndex > 0) ? currentIndex - 1 : totalItems - 1;
-        updateCarousel();
-    });
-
-    nextButton.addEventListener('click', () => {
-        currentIndex = (currentIndex < totalItems - 1) ? currentIndex + 1 : 0;
-        updateCarousel();
-    });
 });
